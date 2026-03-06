200c1c2fafc8b627fc528b081eedbb8e44797eab
Austin Hoover <austin.michael.hoover@gmail.com>
2026-03-06 16:05:17 -0500

    Start adding transfer matrix calculations

--- py/orbit/envelope/envelope.py
+++ py/orbit/envelope/envelope.py
@@ -5,6 +5,11 @@ import numpy as np  # can switch to internal Matrix class later
 from orbit.core.bunch import SyncParticle
 from orbit.lattice import AccLattice
 from orbit.lattice import AccNode
+from orbit.teapot import DriftTEAPOT
+from orbit.teapot import QuadTEAPOT
+from orbit.teapot import TiltTEAPOT
+from orbit.teapot import FringeFieldTEAPOT
+from orbit.utils.consts import speed_of_light
 
 
 ENTRANCE = AccNode.ENTRANCE
@@ -53,7 +58,7 @@ class Envelope:
         self.cov_matrix = np.linalg.multi_dot([matrix_sub, self.cov_matrix, matrix_sub.T])
         self.centroid = np.matmul(matrix, self.centroid)
 
-    
+
 class EnvelopeTracker:
     """Tracks beam envelope/centroid through linear lattice."""
     def __init__(self, lattice: AccLattice) -> None:
@@ -62,31 +67,62 @@ class EnvelopeTracker:
     def addSpaceChargeNodes(self, min_sep: float, max_sep: float) -> None:
         """Add space charge kicks to the lattice as child nodes."""
         raise NotImplementedError
+    
+    def getMatrix(self, node: AccNode, envelope: Envelope, part_index: int = None) -> np.ndarray:
+        """Compute transfer matrix."""
+        matrix = None
+
+        if type(node) is DriftTEAPOT:
+            length = node.getLength(part_index)
+
+            sync_part = envelope.sync_part
+            dp_p_coeff = 1.0 / (sync_part.momentum() * sync_part.beta())
+            dp_p = envelope.centroid[5] * dp_p_coeff
+
+            matrix = np.identity(7)
+            matrix[0, 1] = length / (1.0 + dp_p)
+            matrix[2, 3] = length / (1.0 + dp_p)
+            # TO DO: longitudinal
+
+        elif type(node) is TiltTEAPOT:
+            angle = node.getTiltAngle()
+            cs = np.cos(angle)
+            sn = np.sin(angle)
+            matrix = np.identity(7)
+            matrix[0, 0] = matrix[1, 1] = +cs
+            matrix[0, 2] = matrix[1, 3] = +sn
+            matrix[2, 0] = matrix[3, 1] = -sn
+            matrix[2, 2] = matrix[3, 3] = +cs
+
+        elif type(node) is FringeFieldTEAPOT:
+            matrix = np.identity(7)
+
+        else:
+            raise ValueError(f"No transfer matrix for node {node}")
+
+        return matrix
 
     def track(self, envelope: Envelope) -> None:   
-        """Track envelope through lattice.
-        
-        This assumes all nodes have `getMatrix` method.
-        """
+        """Track envelope through lattice."""
         for node in self.lattice.getNodes():
             for child_node in node.getChildNodes(ENTRANCE):
-                matrix = child_node.getMatrix(envelope)
+                matrix = self.getMatrix(child_node, envelope)
                 envelope.propagate(matrix)
 
             for part_index in range(node.getnParts()):
                 for child_node in node.getChildNodes(BODY, part_index, place_in_part=BEFORE):
-                    matrix = child_node.getMatrix(envelope)
+                    matrix = self.getMatrix(child_node, envelope)
                     envelope.propagate(matrix)
 
-                matrix = node.getMatrix(envelope, part_index=part_index)
+                matrix = self.getMatrix(node, envelope, part_index)
                 envelope.propagate(matrix)
 
                 for child_node in node.getChildNodes(BODY, part_index, place_in_part=AFTER):
-                    matrix = child_node.getMatrix(envelope)
+                    matrix = self.getMatrix(child_node, envelope)
                     envelope.propagate(matrix)
             
             for child_node in node.getChildNodes(EXIT):
-                matrix = child_node.getMatrix(envelope)
+                matrix = self.getMatrix(child_node, envelope)
                 envelope.propagate(matrix)
 
 
@@ -116,3 +152,4 @@ class EnvelopeSpaceChargeKick3D(EnvelopeSpaceChargeKick):
 
     def getMatrix(self, envelope: Envelope) -> None:
         raise NotImplementedError
+    
