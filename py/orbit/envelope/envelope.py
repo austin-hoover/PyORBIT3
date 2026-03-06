4686fc7e1a60aac28551bfda65b70f85f70b2dc2
Austin Hoover <austin.michael.hoover@gmail.com>
2026-03-06 14:10:56 -0500

    Start EnvelopeTracker

--- py/orbit/envelope/envelope.py
+++ py/orbit/envelope/envelope.py
@@ -1,24 +1,32 @@
 import math
 
-# Use NumPy for now; can switch to internal Matrix class later.
-import numpy as np
+import numpy as np  # can switch to internal Matrix class later
 
-from orbit.core.bunch import Bunch
-from orbit.core.bunch import SyncParticle
 from orbit.lattice import AccLattice
 from orbit.lattice import AccNode
 
 
+ENTRANCE = AccNode.ENTRANCE
+BODY = AccNode.BODY
+EXIT = AccNode.EXIT
+
+BEFORE = AccNode.BEFORE
+AFTER = AccNode.AFTER
+
+
 class Envelope:
-    def __init__(self, sync_part: SyncParticle, cov_matrix: np.ndarray, centroid: np.ndarray = None, intensity: float = 0.0) -> None:
+    def __init__(self, mass: float, kin_energy: float, cov_matrix: np.ndarray, centroid: np.ndarray = None, intensity: float = 0.0) -> None:
         """Constructor.
         
-        sync_part: Synchronous particle.
-        cov_matrix: Bunch covariance matrix, shape (6, 6).
-        centroid: Bunch centroid vector, shape (6,).
-        intensity: Number of particles in the bunch.
+        Args:
+            mass: Particle mass [GeV / c^2].
+            kin_energy: Kinetic energy of synchronous particle [GeV].
+            cov_matrix: Bunch covariance matrix, shape (6, 6).
+            centroid: Bunch centroid vector, shape (6,).
+            intensity: Number of particles in the bunch.
         """
-        self.sync_part = sync_part
+        self.mass = mass
+        self.kin_energy = kin_energy
         
         self.cov_matrix = cov_matrix
         
@@ -32,6 +40,12 @@ class Envelope:
 
         self.intensity = intensity
 
+    def propagate(self, matrix: np.ndarray) -> None:
+        """Linear propagation of covariance matrix and centroid."""
+        matrix_sub = matrix[0:6, 0:6]
+        self.cov_matrix = np.linalg.multi_dot([matrix_sub, self.cov_matrix, matrix_sub.T])
+        self.centroid = np.matmul(matrix, self.centroid)
+
     def get_sc_matrix_2d(self, length: float) -> None:
         """Return matrix for linear space charge kick from uniform-density ellipse (2D)."""
         raise NotImplementedError
@@ -42,16 +56,22 @@ class Envelope:
 
 
 class EnvelopeTracker:
-    def __init__(self, lattice: AccLattice, envelope: Envelope) -> None:
+    def __init__(self, lattice: AccLattice) -> None:
         self.lattice = lattice
-        self.envelope = envelope
-
-    def _apply_matrix(self, matrix: np.ndarray) -> None:
-        # Track centroid: x -> Mx.
-        self.centroid = np.matmul(matrix, self.centroid)
-        
-        # Track covariance matrix: S -> M S M^T.
-        matrix_sub = matrix[:6, :6]
-        self.cov_matrix = np.linalg.multi_dot([matrix_sub, self.cov_matrix, matrix_sub.T])
-
     
+    def track(self, envelope: Envelope) -> None:        
+        for node in self.lattice.getNodes():
+            for child_node in node.getChildNodes(ENTRANCE):
+                envelope.propagate(child_node.getMatrix())
+
+            for part_index in range(node.getnParts()):
+                for child_node in node.getChildNodes(BODY, part_index, place_in_part=BEFORE):
+                    envelope.propagate(child_node.getMatrix())
+
+                envelope.propagate(node.getMatrix(part_index))
+
+                for child_node in node.getChildNodes(BODY, part_index, place_in_part=AFTER):
+                    envelope.propagate(child_node.getMatrix())
+            
+            for child_node in node.getChildNodes(EXIT):
+                envelope.propagate(child_node.getMatrix())
