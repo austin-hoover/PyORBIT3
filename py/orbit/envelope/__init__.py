ef80a9098bc1f4c15ac3efeebfbd0f2e1b1cdf07
Austin Hoover <austin.michael.hoover@gmail.com>
2026-03-06 14:32:29 -0500

    Add envelope module

---
+++ py/orbit/envelope/__init__.py
@@ -0,0 +1,5 @@
+from .envelope import Envelope
+from .envelope import EnvelopeTracker
+from .envelope import EnvelopeSpaceChargeKick
+from .envelope import EnvelopeSpaceChargeKick2D
+from .envelope import EnvelopeSpaceChargeKick3D
