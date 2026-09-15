48d43808222976c2f6f970ffa1e3f53ed9940a1c
austin-hoover <austin.michael.hoover@gmail.com>
2026-09-15 18:51:54 -0400

    Just use original calculation and add if statement

--- py/orbit/matrix_lattice/MATRIX_Lattice.py
+++ py/orbit/matrix_lattice/MATRIX_Lattice.py
@@ -247,11 +247,9 @@ class MATRIX_Lattice(AccLattice):
                 track_m.set(2, 2, mt.get(ind1, ind1) * mt.get(ind1, ind1))
                 alpha_0 = track_v.get(0)
                 beta_0 = track_v.get(1)
-
-                upper = mt.get(ind0, ind1)
-                lower = beta_0 * mt.get(ind0, ind0) - alpha_0 * mt.get(ind0, ind1)
-                delta_phi = math.atan2(upper, lower) % (2.0 * math.pi)
-
+                delta_phi = math.atan(mt.get(ind0, ind1) / (beta_0 * mt.get(ind0, ind0) - alpha_0 * mt.get(ind0, ind1)))
+                if delta_phi < 0:
+                    delta_phi += math.pi
                 phi = phi + delta_phi
                 track_v = track_m.mult(track_v)
                 position = position + matrixNode.getLength()
